41772e3e25a6d9ccb6db391b82edabc7d04c60b1
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2016-08-24 15:23:08 -0500

    put right paths into rootlogon

--- rootlogon.C
+++ rootlogon.C
@@ -1,8 +1,8 @@
 {
-  gSystem->Load("libRootFftwWrapper.so");
-  gSystem->Load("libAnitaEvent.so"); 
-  gSystem->Load("libAnitaCorrelator.so"); 
-  gSystem->Load("libAnitaAnalysis.so"); 
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libRootFftwWrapper.so");
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaEvent.so"); 
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaCorrelator.so"); 
+  gSystem->Load("${ANITA_UTIL_INSTALL_DIR}/lib/libAnitaAnalysis.so"); 
   gSystem->Load("build/libUCorrelator.so"); 
 
 
