61a643de6a164433fd9f6641c8806e58b5f34d8c
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-02-02 11:39:22 -0600

    actually make it compile

--- src/ImageTools.cc
+++ src/ImageTools.cc
@@ -16,8 +16,6 @@ static TH1* makeTH1(char type, const char * name, const char * title, int nbins,
       return new TH1S(name,title,nbins,xmin,xmax); 
     case 'I': 
       return new TH1I(name,title,nbins,xmin,xmax); 
-    case 'L': 
-      return new TH1L(name,title,nbins,xmin,xmax); 
     case 'F': 
       return new TH1F(name,title,nbins,xmin,xmax); 
     case 'D': 
@@ -27,6 +25,21 @@ static TH1* makeTH1(char type, const char * name, const char * title, int nbins,
 }
 
 
+static const TAxis * getAxis(const TH2* H, int axis) 
+{
+  switch(axis) 
+  {
+
+    case 1: 
+      return H->GetXaxis(); 
+    case 2: 
+      return H->GetYaxis(); 
+    default:
+      fprintf(stderr,"UCorrelator::image: axis must be 1 (x) or 2 (y)\n"); 
+      return 0; 
+  }
+}
+
 
 TH1* UCorrelator::image::getPctileProjection(const TH2 * H, int axis, double pct) 
 {
@@ -35,9 +48,9 @@ TH1* UCorrelator::image::getPctileProjection(const TH2 * H, int axis, double pct
   name.Form("%s_%g_proj_%d", H->GetName(), pct, axis); 
   title.Form("%s %g%% projection of axis %d", H->GetTitle(), 100*pct, axis); 
   
-  TH1 * h = makeTH1(H->GetClassName()[3], name.Data(), title.Data(); H->GetAxis(axis)->GetNbins(), H->GetAxis(axis)->GetXmin(), H->GetAxis(axis)->GetXmax()); 
+  TH1 * h = makeTH1(H->ClassName()[3], name.Data(), title.Data(), getAxis(H,axis)->GetNbins(), getAxis(H,axis)->GetXmin(), getAxis(H,axis)->GetXmax()); 
 
-  int nOrth = h->GetXaxis(3-axis)->GetNbins(); 
+  int nOrth = getAxis(H,3-axis)->GetNbins(); 
 
   for (int i = 1; i <= h->GetNbinsX(); i++) 
   {
@@ -47,7 +60,7 @@ TH1* UCorrelator::image::getPctileProjection(const TH2 * H, int axis, double pct
       v[j-1] = H->GetBinContent( axis == 1 ? i : j, axis == 1 ? j : i); 
     }
     std::nth_element(v.begin(), v.begin() + (nOrth-1) * pct, v.end()); 
-    h->SetBinContent(i, v[(nOrth-1)*pct]; 
+    h->SetBinContent(i, v[(nOrth-1)*pct]); 
   }
 
   return h; 
