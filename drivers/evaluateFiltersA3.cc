4297f56a4966ce0ef35b45f53567ee5f59da5388
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-04-05 03:18:02 -0500

    add deconvolve + sinsub and geometric filter to filter evaluation framework

--- drivers/evaluateFiltersA3.cc
+++ drivers/evaluateFiltersA3.cc
@@ -20,7 +20,9 @@
 #include "UCFilters.h"
 #include <vector>
 #include <string> 
+#include "SystemResponse.h" 
 #include "BasicFilters.h" 
+#include "GeomFilter.h" 
 #include "AnalysisConfig.h"
 #include "AnitaDataset.h"
 #include "RawAnitaHeader.h"
@@ -41,23 +43,27 @@ std::vector<FilterStrategy*> strategies;
 std::vector<std::string> names; 
 
 void addStrategy( FilterStrategy * s, const char * name) { strategies.push_back(s); names.push_back(name); } 
+UCorrelator::Analyzer * analyzer; 
+UCorrelator::AnalysisConfig cfg; 
 
-UCorrelator::WaveformCombiner wc(12); //this code is so shitty 
 
 /** add filter strategies here ! */ 
 
 void setupFilters(TFile* out, int run) 
 {
+  (void) out; 
   justAlfa.addOperation(new ALFAFilter); 
 
   /** Sine subtraction */ 
   FilterStrategy * sinsub_05_0= new FilterStrategy; 
-  sinsub_05_0->addOperation(new UCorrelator::SineSubtractFilter(0.05, 0)); 
+  UCorrelator::SineSubtractFilter * ssf = new UCorrelator::SineSubtractFilter(0.05, 0); 
+  sinsub_05_0->addOperation(ssf); 
   sinsub_05_0->addOperation(new ALFAFilter); 
   addStrategy(sinsub_05_0, "sinsub_05_0"); 
 
   FilterStrategy * sinsub_03_0= new FilterStrategy; 
-  sinsub_03_0->addOperation(new UCorrelator::SineSubtractFilter(0.03, 0)); 
+  ssf = new UCorrelator::SineSubtractFilter(0.03, 0); 
+  sinsub_03_0->addOperation(ssf); 
   sinsub_03_0->addOperation(new ALFAFilter); 
   addStrategy(sinsub_03_0, "sinsub_03_0"); 
 
@@ -82,12 +88,14 @@ void setupFilters(TFile* out, int run)
   UCorrelator::SineSubtractFilter * adssf = new UCorrelator::SineSubtractFilter(0.05, 0); 
   adssf->makeAdaptive(avg); 
   adsinsub_05_0->addOperation(adssf); 
+//  adssf->setVerbose(1); 
   adsinsub_05_0->addOperation(new ALFAFilter); 
   addStrategy(adsinsub_05_0, "adsinsub_05_0"); 
 
   FilterStrategy * adsinsub_10_0= new FilterStrategy; 
   adssf = new UCorrelator::SineSubtractFilter(0.10, 0); 
   adssf->makeAdaptive(avg); 
+//  adssf->setVerbose(1); 
   adsinsub_10_0->addOperation(adssf); 
   adsinsub_10_0->addOperation(new ALFAFilter); 
   addStrategy(adsinsub_10_0, "adsinsub_10_0"); 
@@ -95,12 +103,33 @@ void setupFilters(TFile* out, int run)
   FilterStrategy * adsinsub_10_3= new FilterStrategy; 
   adssf = new UCorrelator::SineSubtractFilter(0.10, 3); 
   adssf->makeAdaptive(avg); 
+//  adssf->setVerbose(1); 
   adsinsub_10_3->addOperation(adssf); 
   adsinsub_10_3->addOperation(new ALFAFilter); 
   addStrategy(adsinsub_10_3, "adsinsub_10_3"); 
 
 
 
+  /** deconv + sine subtraction */ 
+  FilterStrategy * decon_ss_5_0 = new FilterStrategy; 
+  decon_ss_5_0->addOperation(new UCorrelator::DeconvolveFilter(analyzer->getResponseManager(), cfg.deconvolution_method)); 
+  decon_ss_5_0->addOperation(new ALFAFilter); 
+  decon_ss_5_0->addOperation(new UCorrelator::SineSubtractFilter(0.05,0)); 
+  addStrategy(decon_ss_5_0,"decon_ss_5_0"); 
+
+  FilterStrategy * decon_ss_3_0 = new FilterStrategy; 
+  decon_ss_3_0->addOperation(new UCorrelator::DeconvolveFilter(analyzer->getResponseManager(), cfg.deconvolution_method)); 
+  decon_ss_3_0->addOperation(new ALFAFilter); 
+  decon_ss_3_0->addOperation(new UCorrelator::SineSubtractFilter(0.03,0)); 
+  addStrategy(decon_ss_3_0,"decon_ss_3_0"); 
+
+  FilterStrategy * decon_adss_5_0 = new FilterStrategy; 
+  decon_adss_5_0->addOperation(new UCorrelator::DeconvolveFilter(analyzer->getResponseManager(), cfg.deconvolution_method)); 
+  decon_adss_5_0->addOperation(new ALFAFilter); 
+  adssf = new UCorrelator::SineSubtractFilter(0.05,0); 
+  adssf->makeAdaptive(avg); 
+  decon_adss_5_0->addOperation(adssf); 
+ addStrategy(decon_adss_5_0,"decon_adss_5_0"); 
 
 
   /** Adaptive Butterworth Filter */ 
@@ -116,12 +145,41 @@ void setupFilters(TFile* out, int run)
 
 
 
-  /** Adaptive Minimum Phase **/ 
-  FilterStrategy * minphase = new  FilterStrategy; 
-  minphase->addOperation(new UCorrelator::AdaptiveMinimumPhaseFilter(avg)); 
-  minphase->addOperation(new ALFAFilter); 
-  addStrategy(minphase, "minphase"); 
+  /** Adaptive Minimum Phase exponent = -2**/ 
+  FilterStrategy * minphase_2 = new  FilterStrategy; 
+  minphase_2->addOperation(new UCorrelator::AdaptiveMinimumPhaseFilter(avg,-2)); 
+  minphase_2->addOperation(new ALFAFilter); 
+  addStrategy(minphase_2, "minphase_2"); 
 
+  FilterStrategy * minphase_1 = new  FilterStrategy; 
+  minphase_1->addOperation(new UCorrelator::AdaptiveMinimumPhaseFilter(avg,-1)); 
+  minphase_1->addOperation(new ALFAFilter); 
+  addStrategy(minphase_1, "minphase_1"); 
+
+  /**Geometric Filter */ 
+
+  FilterStrategy * geom = new FilterStrategy; 
+  geom->addOperation(new ALFAFilter); 
+
+  std::vector<std::vector<TGraphAligned* > > noise(48, std::vector<TGraphAligned*>(2)); 
+  for (int ant = 0; ant < 48; ant++)
+  {
+    for (int pol = 0; pol < 2; pol++) 
+    {
+      TH1 * avg = UCorrelator::SpectrumAverage::defaultThermal()->getSpectrumPercentile(AnitaPol::AnitaPol_t(pol), ant,0.5,true); 
+      noise[ant][pol] = new TGraphAligned(avg->GetNbinsX()); 
+      for (int i = 0; i < avg->GetNbinsX(); i++) 
+      {
+        noise[ant][pol]->GetX()[i] = avg->GetBinLowEdge(i+1); 
+        noise[ant][pol]->GetY()[i] = avg->GetBinContent(i+1); 
+      }
+    }
+  }
+
+  FilterOperation * geomfilter = new GeometricFilter(noise); 
+  geom->addOperation(geomfilter); 
+
+  addStrategy(geom,"geom"); 
 
 }
 
@@ -150,30 +208,38 @@ int main(int nargs, char ** args)
   else outname.Form("filter/%d_%s_max_%d.root", run, label, max); 
 
   TFile ofile(outname, "RECREATE"); 
-  setupFilters(&ofile, run); 
-
 
   if (!ofile.IsOpen())
   {
     return 1; 
   }
 
-  UCorrelator::AnalysisConfig cfg; 
-  UCorrelator::Analyzer analyzer(&cfg); 
 
+
+  cfg.response_option=UCorrelator::AnalysisConfig::ResponseIndividualBRotter;
+  cfg.deconvolution_method = new UCorrelator::AllPassDeconvolution; 
+  analyzer = new UCorrelator::Analyzer(&cfg); 
+
+
+
+  setupFilters(&ofile, run); 
+ 
+  UCorrelator::WaveformCombiner wc(12,3,true,true,analyzer->getResponseManager()); //this code is so shitty 
   AnitaEventSummary * sum = new AnitaEventSummary; 
   RawAnitaHeader *hdr = 0 ; 
   UsefulAdu5Pat *patptr = 0; 
 
   std::vector<TTree*> trees(strategies.size()); 
 
-  double pulserH=0, pulserV=0; 
+  double pulserH=0, pulserV=0, pulserDH=0, pulserDV=0; 
 
   TTree * friendly = new TTree("aux", "Auxdata (headers/gps)"); 
   friendly->Branch("header",&hdr); 
   friendly->Branch("pat",&patptr); 
-  friendly->Branch("peakPulserH",&pulserH); 
-  friendly->Branch("peakPulserV",&pulserV); 
+  friendly->Branch("peakPulserCoherentH",&pulserH); 
+  friendly->Branch("peakPulserCoherentV",&pulserV); 
+  friendly->Branch("peakPulserDeconvolvedH",&pulserDH); 
+  friendly->Branch("peakPulserDeconvolvedV",&pulserDV); 
   friendly->SetAutoSave(500); 
 
   for (size_t i = 0; i < strategies.size(); i++) 
@@ -205,8 +271,10 @@ int main(int nargs, char ** args)
          theta *= 180/TMath::Pi(); 
          wc.combine(phi,theta,&ev,AnitaPol::kHorizontal); 
          pulserH = FFTtools::getPeakVal(wc.getCoherent()->hilbertEnvelope()); 
+         pulserDH = FFTtools::getPeakVal(wc.getDeconvolved()->hilbertEnvelope()); 
          wc.combine(phi,theta,&ev,AnitaPol::kVertical); 
          pulserV = FFTtools::getPeakVal(wc.getCoherent()->hilbertEnvelope()); 
+         pulserDV = FFTtools::getPeakVal(wc.getDeconvolved()->hilbertEnvelope()); 
          printf("  Pulser H: %g, Pulser V: %g\n", pulserH, pulserV); 
 
       }
@@ -253,8 +321,10 @@ int main(int nargs, char ** args)
     for (size_t s = 0; s < strategies.size(); s++) 
     {
       printf (" %s...", names[s].c_str()); 
+//  AnalysisWaveform::enableDebug(true); 
       FilteredAnitaEvent ev(d.useful(), strategies[s], d.gps(), d.header()); 
-      analyzer.analyze(&ev, sum); 
+ // AnalysisWaveform::enableDebug(false); 
+      analyzer->analyze(&ev, sum); 
       printf("[%g,%g]",sum->coherent[0][0].peakHilbert, sum->coherent[1][0].peakHilbert);
       ofile.cd(); 
       trees[s]->Fill(); 
