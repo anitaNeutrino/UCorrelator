53699f0ab36c267cb973fad5a0c03b5093d7f601
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-05-08 12:12:22 -0500

    cleanup TMVA macro

--- macro/doTMVA.C
+++ macro/doTMVA.C
@@ -12,7 +12,7 @@ int wais_stop = 362;
 #include "AnitaTMVA.h" 
 
 
-void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filter = "sinsub_5_3_ad_2", int nworkers = 1) 
+void makeTrees(int decimated_start = 130, int decimated_stop=439, const char * filter = "sinsub_5_3_ad_2", int nworkers = 1) 
 {
 
   // Step 1: load data
@@ -43,7 +43,6 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
     proof = TProof::Open("",TString::Format("workers=%d",nworkers)); 
     proof->Load("macro/proofloader.C",true); 
     proof->Load("macro/cuts.C",true); 
-
     signal.SetProof(); 
     bg.SetProof(); 
   }
@@ -52,7 +51,7 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
   //Step 2: set cuts
 
   TCut signal_cut = isWais && isReal; 
-  TCut bg_cut = thermal_sample && brightestPeak && notTooFiltered; 
+  TCut bg_cut = thermal_sample && brightestPeak; 
 
   AnitaTMVA::MVAVarSet varset; 
   //setup variables
@@ -72,10 +71,26 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
   varset.add(AnitaTMVA::MVAVar("deconvolved.width_50_50[][]","deconvolvedWidth5050")); 
   varset.add(AnitaTMVA::MVAVar("deconvolved.width_10_10[][]","deconvolvedWidth1010")); 
   varset.add(AnitaTMVA::MVAVar("deconvolved.peakTime[][]","deconvolvedPeakTime")); 
+  varset.add(AnitaTMVA::MVAVar("abs(FFTtools::wrap(peak.phi-sun.phi,360,0))","dPhiSun")); 
+  varset.add(AnitaTMVA::MVAVar("abs(FFTtools::wrap(peak.phi-heading,360,0))","dPhiNorth")); 
+  varset.add(AnitaTMVA::MVAVar("flags.meanPowerFiltered[0] / flags.meanPower[0]","filteredFraction")); 
 
   //add spectactors 
   varset.add(AnitaTMVA::MVAVar("run","run",'I',true)); 
   varset.add(AnitaTMVA::MVAVar("eventNumber","eventNumber",'I',true)); 
+
+  TString treefilename; 
+  treefilename.Form("thermalCutTrees_%s.root",filter); 
+
+
+  TFile newOut(treefilename.Data(),"RECREATE"); 
+  TTree * sigtree= AnitaTMVA::makeTMVATree(&signal, &newOut,  "signal_in", varset, signal_cut); 
+  TTree * bgtree= AnitaTMVA::makeTMVATree(&bg, &newOut,  "bg_in", varset, bg_cut); 
+  newOut.Write(); 
+}
+
+void doTMVA(int decimated_start = 160, int decimated_stop=439, const char * filter = "sinsub_10_3_ad_2", int nworkers = 1) 
+{
 
 
   TString treefilename; 
@@ -86,10 +101,7 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
 
   if (!f) 
   {
-    TFile newOut(treefilename.Data(),"CREATE"); 
-    TTree * sigtree= AnitaTMVA::makeTMVATree(&signal, &newOut,  "signal_in", varset, signal_cut); 
-    TTree * bgtree= AnitaTMVA::makeTMVATree(&bg, &newOut,  "bg_in", varset, bg_cut); 
-    newOut.Write(); 
+    makeTrees(decimated_start, decimated_stop, filter, nworkers); 
   }
   else
   {
@@ -98,34 +110,32 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
 
 
 
-  
   TFile  out(treefilename.Data()); 
   TTree* sigtree = (TTree*) out.Get("signal_in"); 
   TTree* bgtree = (TTree*) out.Get("bg_in"); 
-  
-
 
   TString tmvaOutName; 
   tmvaOutName.Form("thermalCuts_%s.root",filter); 
 
   TFile tmvaOut(tmvaOutName.Data(),"RECREATE"); 
-  // Step 3: setup TMVA  
+
   TMVA::Factory *factory = new TMVA::Factory("thermal_cuts", &tmvaOut,"V"); 
 
-
   TMVA::DataLoader *dl = new TMVA::DataLoader("thermal"); 
-
-
-  /// this is where the input selection cuts  are set now until I can figure out the entrylist
   
+
+  /* These are the variables to be used. They must have been generated already */ 
+  dl->AddVariable("mapPeak"); 
+  dl->AddVariable("coherentHilbertPeak"); 
+  dl->AddVariable("deconvHilbertPeak"); 
+  dl->AddVariable("dPhiSun"); 
+  dl->AddSpectator("run"); 
+  dl->AddSpectator("eventNumber"); 
   dl->AddSignalTree(sigtree); 
   dl->AddBackgroundTree(bgtree); 
 
-  varset.setUpData(dl); 
-
 
   //setup methods 
-
   factory->BookMethod(dl, TMVA::Types::kFisher, "Fisher","CreateMVAPdfs=true"); 
 //  factory->BookMethod(dl, TMVA::Types::kKNN, "kNN"); 
   factory->BookMethod(dl, TMVA::Types::kBDT, "BDT","CreateMVAPdfs=true"); 
@@ -138,10 +148,6 @@ void doTMVA(int decimated_start = 130, int decimated_stop=439, const char * filt
   factory->EvaluateAllMethods(); 
 
   tmvaOut.Write(); 
-
-
-
-
 
 
 }
