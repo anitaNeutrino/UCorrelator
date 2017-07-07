5b99a9510992e9d067bff61091869903f0f04216
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-07-07 16:35:29 -0500

    modernize doWais macro

--- macro/doWais.C
+++ macro/doWais.C
@@ -9,12 +9,11 @@
 #include "Util.h"
 #include "FilterStrategy.h"
 
-void doWais(int run = 352, int max = 0, bool sine_subtract = true)
+void doWais(int run = 352, int max = 0, const char * filter = "sinsub_10_3_ad_2")
 {
 
   FFTtools::loadWisdom("wisdom.dat"); 
 
-//  /*AnalysisWaveform::InterpolationType*/ AnalysisWaveform::defaultInterpolationType = AnalysisWaveform::REGULARIZED_SPARSE_YEN; 
 
   AnitaDataset d(run); 
   UCorrelator::AnalysisConfig cfg; 
@@ -25,29 +24,20 @@ void doWais(int run = 352, int max = 0, bool sine_subtract = true)
   UCorrelator::Analyzer analyzer(&cfg); 
 
   TString outname; 
-  if (max) outname.Form("wais/wais_hpol_%d_max_%d%s.root",run,max, sine_subtract ? "_sinsub" : "" ); 
-  else outname.Form("wais/wais_hpol_%d%s.root",run, sine_subtract ? "_sinsub" : "" ); 
+  if (max && start) outname.Form("wais/%d_max_%d_start_%d_%s.root",run,max,start,filter); 
+  else if (max) outname.Form("wais/%d_max_%d_%s.root",run,max,filter); 
+  else if (start) outname.Form("wais/%d_start_%d_%s.root",run,start,filter); 
+  else outname.Form("wais/%d_%s.root",run, filter); 
+
 
   TFile ofile(outname, "RECREATE"); 
+
   TTree * tree = new TTree("wais","WAIS Hpol"); 
   AnitaEventSummary * sum = new AnitaEventSummary; 
 
 
   FilterStrategy strategy (&ofile); 
-  if (sine_subtract) 
-  {
-    double fmins[1] = {0.2}; 
-    double fmaxs[1] = {1.3}; 
-    strategy.addOperation(new UCorrelator::SineSubtractFilter(0.05, 0, 1,fmins,fmaxs)); 
-    strategy.addOperation(new SimplePassBandFilter(0.2,1.3)); 
-    strategy.addOperation(new ALFAFilter); 
-  }
-  else
-  {
-    UCorrelator::applyAbbysFilterStrategy(&strategy); 
-  }
-
-//  printf("Strategy applied!\n"); 
+  UCorrelator::fillStrategyWithKey(&strategy, filter); 
 
   RawAnitaHeader *hdr = 0 ; 
   UsefulAdu5Pat *patptr = 0; 
@@ -72,7 +62,6 @@ void doWais(int run = 352, int max = 0, bool sine_subtract = true)
       analyzer.analyze(&ev, sum); 
       ofile.cd(); 
       hdr = d.header();
-//      header = d.header(); 
       patptr = &pat; 
       tree->Fill(); 
       ndone++; 
