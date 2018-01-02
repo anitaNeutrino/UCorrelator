3ff7c547e75d22133671981b5a90b3270179f742
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-01-02 13:09:32 -0600

    have both aug and dec mc in plotFisher

--- macro/plotFisher.C
+++ macro/plotFisher.C
@@ -1,33 +1,44 @@
 void plotFisher(const char * var = "F")
 {
-  TChain csim("simulation"); 
-  csim.Add("thermalTrees/simulated*.root"); 
+  TChain caugsim("simulation"); 
+  caugsim.Add("thermalTrees/simulated*.root"); 
+
+  TChain cdecsim("simulation"); 
+  cdecsim.Add("thermalTrees/fakenoise*root"); 
 
   TChain cbg("anita3"); 
   cbg.Add("thermalTrees/a3all*.root"); 
-  cbg.SetProof(1); 
+//  cbg.SetProof(1); 
 
-  csim.SetLineColor(4); 
+  caugsim.SetLineColor(4); 
   cbg.SetLineColor(2); 
+  cdecsim.SetLineColor(3); 
 
   TFile fisher_plots("fisher_plots.root","RECREATE");
   TH1D * hbg = new TH1D("hbg","Upward-Pointing Thermals That Pass Quality Cuts",200,-10,10); 
-  TH1D * hsim = new TH1D("hsim","Weighted MC Pass Quality Cuts",200,-10,10); 
+  TH1D * haugsim = new TH1D("haugsim","Weighted Aug17 MC Pass Quality Cuts",200,-10,10); 
+  TH1D * hdecsim = new TH1D("hdecsim","Weighted Dec17 MC Pass Quality Cuts",200,-10,10); 
 
   TCanvas * dists = new TCanvas(); 
 
   cbg.Draw("F >> hbg",
-              "isMostImpulsive * (chisq < 0.01) * (MaxPeak < 1000) * (theta < 0)  * (!payloadBlast) *  ((iteration < 5 && HPolTrigger) || (iteration > 4 && VPolTrigger))", 
+              "isMostImpulsive *  (MaxPeak < 1000) * (theta < 0)  * (!payloadBlast) *  ((iteration < 5 && HPolTrigger) || (iteration > 4 && VPolTrigger))", 
              ""); 
 
-  csim.Draw("F >> hsim",
-             "weight * isMostImpulsive * (chisq < 0.01) * (MaxPeak < 1000) * pointsToMC * (!payloadBlast) *  ((iteration < 5 && HPolTrigger) || (iteration > 4 && VPolTrigger))", 
+  caugsim.Draw("F >> haugsim",
+             "weight * isMostImpulsive * (MaxPeak < 1000) * pointsToMC * (!payloadBlast) *  ((iteration < 5 && HPolTrigger) || (iteration > 4 && VPolTrigger))", 
              "same"); 
+
+  cdecsim.Draw("F >> hdecsim",
+             "weight * isMostImpulsive * (MaxPeak < 1000) * pointsToMC * (!payloadBlast) *  ((iteration < 5 && HPolTrigger) || (iteration > 4 && VPolTrigger))", 
+             "same"); 
+
 
 
   fisher_plots.cd(); 
   hbg->Write(); 
-  hsim->Write(); 
+  hdecsim->Write(); 
+  haugsim->Write(); 
 
 
 }
